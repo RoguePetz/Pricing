2eb86f552e17559e4cc4eb8ff623aadb2b8a0e27
RoguePetz <briggsrhema@gmail.com>
2024-02-08 00:30:33 +0100

    made coin boxes

--- script.js
+++ script.js
@@ -13,3 +13,17 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
     navMenu.classList.remove("active");
 
 }))
+
+/*Set up for coingeckoAPI*/
+
+fetch("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd")
+    .then(res => {
+        if (res.ok){
+            console.log("success")
+        }else{
+            console.log("not successful")
+        }
+        res.json
+    })
+    .then(data => console.log(data))
+    .catch(error => console.log("ERROR"))
