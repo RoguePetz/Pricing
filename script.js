d30916ed277d648fa6b372a12c101296f6148868
RoguePetz <briggsrhema@gmail.com>
2024-02-08 01:16:14 +0100

    fetched data

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+/*Set up for coingeckoAPI*/
+
+
+const fetchData = async () => {
+    const response = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Clitecoin%2Cdogecoin&vs_currencies=usd');
+    const data = await response.json();
+    return data;
+}
+
+fetchData()
+    .then(data => console.log('resolved:', data));
+
+
 /*Hamburger menu functionality*/ 
 
 const hamburger = document.querySelector(".hamburger");
@@ -14,16 +27,3 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
 
 }))
 
-/*Set up for coingeckoAPI*/
-
-fetch("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd")
-    .then(res => {
-        if (res.ok){
-            console.log("success")
-        }else{
-            console.log("not successful")
-        }
-        res.json
-    })
-    .then(data => console.log(data))
-    .catch(error => console.log("ERROR"))
